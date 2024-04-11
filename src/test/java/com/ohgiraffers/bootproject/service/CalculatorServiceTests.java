11b6317264e1021a6550025b008cf4ca105f2504
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2024-04-11 12:28:28 +0900

    테스트 코드 정상화

--- src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
+++ src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
@@ -35,7 +35,7 @@ class CalculatorServiceTests {
     void testPlus(CalculatorDTO input) {
 
         // when
-        int expected = input.getNum1() + input.getNum2() + 1;
+        int expected = input.getNum1() + input.getNum2();
 
         // then
         int actual = calculatorService.plus(input);
